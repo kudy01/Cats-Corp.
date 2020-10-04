8492954fc8a409c1a8f80b025e0ef6644d65872d
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-10-04 16:58:09 +0530

    Error handling and Scroll box added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import CatList from './components/CatList';
 import Searchbox from './components/Searchbox';
+import Scroll from './components/scroll';
+import ErrorBoundary from './components/ErrorBoundary';
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "./theme.js";
@@ -32,18 +34,23 @@ class App extends Component {
     const filteredCats = cats.filter(cat => {
       return cat.name.toLowerCase().includes(searchField.toLowerCase())
     })
-    return(
+    return !cats.length ?  // when the page takes time to load as all the names come from an api
+      <h1>Loading</h1>:
+    (
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="App">
             <h1>Cats Corp.</h1>
             <Searchbox searchChange= {this.onSearchChange}/>
-            <CatList cats={filteredCats}/>
+            <Scroll>
+              <ErrorBoundary>
+                <CatList cats={filteredCats}/>
+              </ErrorBoundary>
+            </Scroll>
         </div>
       </ThemeProvider>
-      )  
+      );
   }
 }
-
 
 export default App;
